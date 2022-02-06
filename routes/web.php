8f3d502b48f11acb9b709fcd6f07d69498c03b61
RyutaNoguchi <ryuta.noguchi.levtechcollege@gmail.com>
2022-02-06 17:06:16 +0900

    投稿の編集機能の実装

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
+Route::get('posts/{post}/edit', 'PostController@edit');
 Route::get('/posts', 'PostController@index');
 
 Route::post('/posts', 'PostController@store');
+
+Route::put('/posts/{post}', 'PostController@update');
